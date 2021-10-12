a36e01fe402e291021341461db80d65593ff9a3b
mattioiscattio <mattioiscattio@gmail.com>
2021-10-12 20:10:11 +0100

    completed up to 4.4 push

--- Week 4/Homework 4.3/Homework 4.3/Form1.Designer.cs
+++ Week 4/Homework 4.3/Homework 4.3/Form1.Designer.cs
@@ -29,13 +29,34 @@ namespace Homework_4._3
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.LBOutput = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // LBOutput
+            // 
+            this.LBOutput.FormattingEnabled = true;
+            this.LBOutput.ItemHeight = 15;
+            this.LBOutput.Location = new System.Drawing.Point(12, 12);
+            this.LBOutput.Name = "LBOutput";
+            this.LBOutput.Size = new System.Drawing.Size(514, 304);
+            this.LBOutput.TabIndex = 0;
+            this.LBOutput.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LBOutput);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox LBOutput;
     }
 }
 
--- Week 4/Homework 4.3/Homework 4.3/Form1.cs
+++ Week 4/Homework 4.3/Homework 4.3/Form1.cs
@@ -15,7 +15,21 @@ namespace Homework_4._3
         public Form1()
         {
             InitializeComponent();
+            LBOutput.MultiColumn = true;
+            for (int i = 1; i < 21; i++ )
+            {
+                double output = Math.Round(((i) * 2.2), 2);
+                LBOutput.Items.Add(output.ToString() + " Pounds");
+            }
+            for (int i = 1; i < 21; i++)
+            {
+                LBOutput.Items.Add(i.ToString() + " KG");
+            }
         }
 
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }

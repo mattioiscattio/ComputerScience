237b82cba41b704b74be29de82ffd228e92daa20
Matt Lyne (Student) <ml331961@truro-penwith.ac.uk>
2021-12-16 10:42:32 +0000

    basic code strucutre and most of file structure created

---
+++ Projects/YearOneProjectOne/Program/YearOneProjectOne/YearOneProjectOne/studentView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YearOneProjectOne
+{
+    public partial class studentView : Form
+    {
+        public studentView()
+        {
+            InitializeComponent();
+        }
+    }
+}
